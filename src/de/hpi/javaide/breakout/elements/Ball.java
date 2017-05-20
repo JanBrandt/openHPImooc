079237cbf9975b65ceac5e86d938c38741264eec
hrazut <tod.space@gmx.net>
2017-05-20 22:18:51 +0200

    Added method move and constructor to Ball

--- src/de/hpi/javaide/breakout/elements/Ball.java
+++ src/de/hpi/javaide/breakout/elements/Ball.java
@@ -1,6 +1,7 @@
 package de.hpi.javaide.breakout.elements;
 
 import de.hpi.javaide.breakout.basics.Elliptic;
+import de.hpi.javaide.breakout.basics.Vector;
 import de.hpi.javaide.breakout.starter.Game;
 import java.awt.Dimension;
 import java.awt.Point;
@@ -20,15 +21,21 @@ import java.awt.Point;
 // früher oder später
 // jemand wissen wollen in welche Richtung er fliegt.
 public class Ball extends Elliptic {
+  
+  private int speed;
+  private Vector vector;
 
   public Ball(final Game game, final Point position) {
     super(game, position, new Dimension(10, 10));
-
+    speed = 1;
+    vector.setX(0);
+    vector.setY(1);
   }
 
   public void move() {
-    // TODO Auto-generated method stub
-    
+    int newX = (int) Math.round(position.getX() + speed * vector.getX());
+    int newY = (int) Math.round(position.getY() + speed * vector.getY());
+    update(new Point(newX, newY), dimension);
   }
 
   @Override
