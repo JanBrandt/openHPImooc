85f3e7fd31c2d390b90489397c3b407ada3897c4
JanBrandt <jbrandt1@freenet.de>
2017-05-21 13:31:34 +0200

    Added new constants for wall

--- src/de/hpi/javaide/breakout/starter/GameConstants.java
+++ src/de/hpi/javaide/breakout/starter/GameConstants.java
@@ -3,8 +3,10 @@ package de.hpi.javaide.breakout.starter;
 import java.awt.Point;
 
 public interface GameConstants {
-  int LIVES = 3;
-  int SCREEN_X = 800;
-  int SCREEN_Y = 600;
-  Point STARTPOSITION = new Point(SCREEN_X / 2, SCREEN_Y / 2);
+  final int LIVES = 3;
+  final int NUMBER_OF_BRICKROWS = 6;
+  final int NUMBER_OF_BRICKCOLUMNS = 7;
+  final int SCREEN_X = 800;
+  final int SCREEN_Y = 600;
+  final Point STARTPOSITION = new Point(SCREEN_X / 2, SCREEN_Y / 2);
 }
