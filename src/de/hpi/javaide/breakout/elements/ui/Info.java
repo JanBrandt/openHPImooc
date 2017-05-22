194c8ee9c277c63bf17c9e0bc7ace411731e28ef
JanBrandt <jbrandt1@freenet.de>
2017-05-22 18:50:39 +0200

    Minor changes, comments and corrected whitespaces

--- src/de/hpi/javaide/breakout/elements/ui/Info.java
+++ src/de/hpi/javaide/breakout/elements/ui/Info.java
@@ -4,9 +4,6 @@ import de.hpi.javaide.breakout.basics.Font;
 import de.hpi.javaide.breakout.basics.UIObject;
 import de.hpi.javaide.breakout.starter.Game;
 
-//TODO nach dem die fehlenden Methoden ergänzt wurden, muss hier noch ein Konstruktorparameter 
-//     an das zugehörige Attribut übergeben werden.
-
 public class Info extends UIObject {
 
   private String content;
@@ -24,7 +21,6 @@ public class Info extends UIObject {
 
   @Override
   public void update(String input) {
-    // TODO Auto-generated method stub
-    
+    // nothing to do  
   }
 }
